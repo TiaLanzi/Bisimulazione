3a26a84448de06652c7920de6691c2d4bc4cecbd
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-06-23 10:59:40 +0200

    :art: Created MatchmakingRoom activity

---
+++ app/src/main/java/com/example/bisimulazione/MatchmakingRoom.java
@@ -0,0 +1,14 @@
+package com.example.bisimulazione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchmakingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matchmaking_room);
+    }
+}
