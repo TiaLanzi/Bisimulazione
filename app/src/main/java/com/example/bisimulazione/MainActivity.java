ddb94e3e9a9074df7ce77fd0ee179384bd1a69d7
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-07-10 16:31:24 +0200

    :lipstick: Rendered icons in NavigationDrawer

--- app/src/main/java/com/example/bisimulazione/MainActivity.java
+++ app/src/main/java/com/example/bisimulazione/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -16,11 +17,13 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,6 +31,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
+
+import info.androidhive.fontawesome.FontDrawable;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -65,14 +70,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
+                getSupportActionBar().setTitle(getString(R.string.menu_home));
+                navigateTo(new Home());
                 break;
             case R.id.navigation_bug_report:
+                getSupportActionBar().setTitle(getString(R.string.menu_bug_report));
                 Intent intentBugReport = new Intent(this, BugReport.class);
                 startActivity(intentBugReport);
                 break;
             case R.id.navigation_faq:
+                getSupportActionBar().setTitle(getString(R.string.menu_faq));
                 Intent intentFaq = new Intent(this, FAQ.class);
                 startActivity(intentFaq);
+                break;
+            case R.id.navigation_game:
+                getSupportActionBar().setTitle(getString(R.string.menu_play_game));
+                Intent intentGame = new Intent(this, MatchmakingRoom.class);
+                startActivity(intentGame);
+                break;
+            default:
                 break;
         }
 
@@ -89,6 +105,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void initDrawerLayout() {
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        int[] iconsNavigation = {R.string.fa_home_solid, R.string.fa_user_solid, R.string.fa_play_solid};
+        renderMenuIcons(navigationView.getMenu().getItem(0).getSubMenu(), iconsNavigation, true, false);
+
+        int[] iconsOther = {R.string.fa_question_circle_solid, R.string.fa_cog_solid, R.string.fa_bug_solid};
+        renderMenuIcons(navigationView.getMenu().getItem(1).getSubMenu(), iconsOther, true, false);
+    }
+
+    private void renderMenuIcons(Menu menu, int[] icons, boolean isSolid, boolean isBrand) {
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            if (!menuItem.hasSubMenu()) {
+                FontDrawable fontDrawable = new FontDrawable(this, icons[i], isSolid, isBrand);
+                fontDrawable.setTextColor(ContextCompat.getColor(this, R.color.black));
+                fontDrawable.setTextSize(20);
+                menu.getItem(i).setIcon(fontDrawable);
+            }
+        }
     }
 
     private void navigateTo(Fragment fragment) {
