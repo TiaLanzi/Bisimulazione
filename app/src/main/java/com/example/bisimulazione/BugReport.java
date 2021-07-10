60d63eae48b8853b82f623acb8315afabb21ee31
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-07-10 16:38:05 +0200

    :sparkles: Added navigation between activities

--- app/src/main/java/com/example/bisimulazione/BugReport.java
+++ app/src/main/java/com/example/bisimulazione/BugReport.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,5 +50,13 @@ public class BugReport extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/bisimulazione/FAQ.java
+++ app/src/main/java/com/example/bisimulazione/FAQ.java
@@ -3,6 +3,7 @@ package com.example.bisimulazione;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class FAQ extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class FAQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        getSupportActionBar().setTitle(getString(R.string.faq));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return true;
     }
 }
