bdb39076185273301db66a3f3cd0a6b160df6459
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-07-11 15:42:05 +0200

    :sparkles: Implemented multilanguage

--- app/src/main/java/com/example/bisimulazione/Settings.java
+++ app/src/main/java/com/example/bisimulazione/Settings.java
@@ -4,16 +4,23 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.Locale;
 
 public class Settings extends AppCompatActivity {
+
+    private boolean initialized = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +29,44 @@ public class Settings extends AppCompatActivity {
 
         getSupportActionBar().setTitle(getString(R.string.settings));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final String[] LANGUAGES = {getString(R.string.language_select), getString(R.string.language_italian), getString(R.string.language_english)};
+        Spinner spinner = findViewById(R.id.settings_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, LANGUAGES);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (!initialized) {
+                    initialized = true;
+                    return;
+                }
+                String language = (String) parentView.getItemAtPosition(position);
+                if (language.equalsIgnoreCase(getString(R.string.language_english))) {
+                    changeLanguage("en");
+                } else {
+                    changeLanguage("it");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void changeLanguage(Activity activity, String languageCode) {
-        Locale locale = new Locale(languageCode);
+    private void changeLanguage(String language) {
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        Locale locale = new Locale(language);
         Locale.setDefault(locale);
-        Resources resources = activity.getResources();
-        Configuration configuration = resources.getConfiguration();
-        configuration.setLocale(locale);
-        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent changeLanguage = new Intent(getApplicationContext(), MainActivity.class);
+        changeLanguage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(changeLanguage);
     }
 
     @Override
