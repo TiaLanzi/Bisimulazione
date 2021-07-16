ea57323e680e614d9a2482b1c692f26c922716f1
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-07-16 14:58:24 +0200

    :sparkles: Implemented FAQ

--- app/src/main/java/com/example/bisimulazione/FAQ.java
+++ app/src/main/java/com/example/bisimulazione/FAQ.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import info.androidhive.fontawesome.FontTextView;
 
 public class FAQ extends AppCompatActivity {
 
@@ -12,9 +19,39 @@ public class FAQ extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
 
-        getSupportActionBar().setTitle(getString(R.string.faq));
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.faq));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final boolean[] VISIBILITIES = {false, false, false, false};
+
+        int[] headers = {R.id.faq_header_one, R.id.faq_header_two, R.id.faq_header_three, R.id.faq_header_four};
+
+        int[] answers = {R.id.faq_answer_one, R.id.faq_answer_two, R.id.faq_answer_three, R.id.faq_answer_four};
+
+        int[] toggles = {R.id.faq_toggle_one, R.id.faq_toggle_two, R.id.faq_toggle_three, R.id.faq_toggle_four};
+
+        for (int i = 0; i < answers.length; i++) {
+            final TextView answer = findViewById(answers[i]);
+            final RelativeLayout header = findViewById(headers[i]);
+            final FontTextView toggle = findViewById(toggles[i]);
+
+            final int index = i;
+
+            header.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (VISIBILITIES[index]) {
+                        answer.setVisibility(View.GONE);
+                        toggle.setText(R.string.fa_caret_down_solid);
+                    } else {
+                        answer.setVisibility(View.VISIBLE);
+                        toggle.setText(R.string.fa_caret_up_solid);
+                    }
+                    VISIBILITIES[index] = !VISIBILITIES[index];
+                }
+            });
+        }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
