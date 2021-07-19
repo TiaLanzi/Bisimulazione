13f1baf18f8b5426b32656db05aaa3e182dd8f9e
TiaLanzi <m.lanzi8@campus.unimib.it>
2021-07-19 14:40:12 +0200

    :rocket: Added navigation to MyAccount

--- app/src/main/java/com/example/bisimulazione/MainActivity.java
+++ app/src/main/java/com/example/bisimulazione/MainActivity.java
@@ -68,25 +68,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_home));
                 navigateTo(new Home());
                 break;
-            case R.id.navigation_bug_report:
-                Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_bug_report));
-                Intent intentBugReport = new Intent(this, BugReport.class);
-                startActivity(intentBugReport);
-                break;
-            case R.id.navigation_faq:
-                Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_faq));
-                Intent intentFaq = new Intent(this, FAQ.class);
-                startActivity(intentFaq);
+            case R.id.navigation_my_account:
+                Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.my_account));
+                Intent intentMyAccount = new Intent(this, MyAccount.class);
+                startActivity(intentMyAccount);
                 break;
             case R.id.navigation_game:
                 Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_play_game));
                 Intent intentGame = new Intent(this, MatchmakingRoom.class);
                 startActivity(intentGame);
                 break;
+            case R.id.navigation_faq:
+                Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_faq));
+                Intent intentFaq = new Intent(this, FAQ.class);
+                startActivity(intentFaq);
+                break;
             case R.id.navigation_settings:
                 Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_settings));
                 Intent intentSettings = new Intent(this, Settings.class);
                 startActivity(intentSettings);
+                break;
+            case R.id.navigation_bug_report:
+                Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.menu_bug_report));
+                Intent intentBugReport = new Intent(this, BugReport.class);
+                startActivity(intentBugReport);
                 break;
             default:
                 break;
